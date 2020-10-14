499f04c1ac00e2bf2b4d5af19724b74a274c28a1
Michael-blaze <slashugaming@gmail.com>
2020-10-14 17:19:50 +0300

    removed redundant whitespaces

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from Game import Game
 
-
 def main():
 	game = Game()
 	
 	game.init()
-
 	while True:
-
 		while not game.over():
 			game.input()
 			game.draw()
-
 		game.reset()
 		
-
 if __name__ == '__main__':
 	main()
